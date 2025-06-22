ebe7e43d8d3fc4cc777af4c3931eaf5cffcfeffe
alexlu07 <alexlu.manhasset@gmail.com>
2025-06-22 16:32:31 -0400

    navbar key fix

--- src/renderer/src/components/Navbar/Navbar.tsx
+++ src/renderer/src/components/Navbar/Navbar.tsx
@@ -2,29 +2,32 @@ import './Navbar.css';
 
 import { Link, useLocation } from 'react-router';
 import { PiCpuFill, PiHouseFill } from 'react-icons/pi';
+import { Fragment } from 'react';
 
 export const Navbar = (): React.JSX.Element => {
   const location = useLocation().pathname;
 
   const pages = [
-    { path: '/', icon: <PiHouseFill className="nav-icon" /> },
-    { path: '/train', icon: <PiCpuFill className="nav-icon" /> },
-    { path: '/train', icon: <PiCpuFill className="nav-icon" /> },
+    { path: '/', Icon: PiHouseFill },
+    { path: '/train', Icon: PiCpuFill },
+    { path: '/train', Icon: PiCpuFill },
   ];
 
   return (
     <nav className="navbar">
-      {pages.map(({ path, icon }, idx) => {
+      {pages.map(({ path, Icon }, idx) => {
         const selected = path === location;
         const prevSelected = idx > 0 && pages[idx - 1].path === location;
 
         return (
-          <>
+          <Fragment key={idx}>
             {idx > 0 && !selected && !prevSelected && <div className="nav-divider" />}
-            <Link to={path} key={idx} className={'nav-button' + (selected ? ' selected' : '')}>
-              <div className="nav-square">{icon}</div>
+            <Link to={path} className={'nav-button' + (selected ? ' selected' : '')}>
+              <div className="nav-square">
+                <Icon className="nav-icon" />
+              </div>
             </Link>
-          </>
+          </Fragment>
         );
       })}
     </nav>
